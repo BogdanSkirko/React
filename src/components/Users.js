a22a87eb85bc1f847a1705fcd66ccd67ed2298d0
Bohdan13011991 <Bogdanskirko@gmail.com>
2021-06-11 18:15:01 +0300

    lesson2

---
+++ src/components/Users.js
@@ -0,0 +1,10 @@
+export default function Users(props) {
+    let {name, age, color} = props;
+    return (
+        <div>
+        <h2>{name}</h2>
+        <p>{age}</p>
+        <p>{color}</p>
+</div>
+    );
+}
