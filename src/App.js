9b3fdd52f01296e05c57a7c7efdce3bca667c3b3
Bohdan13011991 <Bogdanskirko@gmail.com>
2021-06-11 18:37:14 +0300

    lesson2

--- src/App.js
+++ src/App.js
@@ -2,11 +2,26 @@ import Users from "./components/Users";
 
 
 export default function App() {
+    let users = [
+        {name: 'vasya', age: 31, isMarried: false},
+        {name: 'petya', age: 30, isMarried: true},
+        {name: 'kolya', age: 29, isMarried: true},
+        {name: 'olya', age: 28, isMarried: false},
+        {name: 'max', age: 30, isMarried: true},
+        {name: 'anya', age: 31, isMarried: false},
+        {name: 'oleg', age: 28, isMarried: false},
+        {name: 'andrey', age: 29, isMarried: true},
+        {name: 'masha', age: 30, isMarried: true},
+        {name: 'olya', age: 31, isMarried: false},
+        {name: 'max', age: 31, isMarried: true}
+    ]
+
     return (
         <div>
-          <Users name={`Ann`} age={30} color={`green`}/>
-          <Users name={`Dima`} age={33} color={`blonde`}/>
-          <Users name={`Bohdan`} age={24} color={`black`}/>
-</div>
-)
+            {
+                users.map((value, index) => <h3 key={index}>{value.name} - {value.age} - {value.isMarried.toString()}</h3>)
+
+            }
+        </div>
+    )
 }
