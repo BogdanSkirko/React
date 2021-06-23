375a3181f2c71f7822a9f28ac75b8dec252a67de
Bohdan13011991 <Bogdanskirko@gmail.com>
2021-06-23 13:01:04 +0300

    homework

--- src/App.js
+++ src/App.js
@@ -1,13 +1,7 @@
-import Users from "./components/charactherComponent";
-
 export default function App() {
     return (
         <div>
-        <Users name={`Bohdan`} age={30} color={`black`}/>
-            <Users name={`Valentyna`} age={64} color={`black`}/>
-            <Users name={`Ann`} age={33} color={`brown`}/>
-            <Users name={`Dmytro`} age={15} color={`white`}/>
-            <Users name={`Yaroslav`} age={69} color={`black`}/>
+
 
         </div>
 
