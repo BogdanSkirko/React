4534dbff4e112831d60b38651fd01365bba02814
Bohdan13011991 <Bogdanskirko@gmail.com>
2021-07-07 20:03:30 +0300

    redux-2

--- src/App.js
+++ src/App.js
@@ -1,48 +1,85 @@
-import {useDispatch, useSelector} from "react-redux";
+// import {useDispatch, useSelector} from "react-redux";
+//
+//
+// const NestedChild = () => {
+//     const counter = useSelector(({counter: {value}}) => value)
+//     const dispatch = useDispatch()
+//     let input = []
+//     // const inputInfo = []
+//     // dispatch({
+//     //     type: 'INC_CUSTOM'
+//     // })
+//  return (
+//     <header className="App-header">
+//       <h1>{counter}</h1>
+//         <button onClick={() => {
+//         dispatch({type: 'INC'})}
+//         }>inc</button>
+//
+//         <button onClick={() => {
+//         dispatch({type: 'DEC'})}
+//         }>DEC</button>
+//
+//         <button onClick={() => {
+//         dispatch({type: 'RESET'})}
+//         }>RESET</button>
+//
+//         <input/>
+//
+//          <button onClick={() => {
+//         dispatch({type: 'INC_CUSTOM'})}
+//         }>INC_CUSTOM</button>
+//
+//     </header>
+//   )
+// }
+//
+//
+//
+// function App() {
+//
+//     return (
+//         <div>
+//             <NestedChild/>
+//         </div>
+//
+//     );
+// }
+// export default App
 
+import {BrowserRouter as Router, Link, Route} from "react-router-dom";
+import Users from "./Users/Users";
+import Posts from "./Posts/Posts";
+import Comments from "./Comments/Comments";
+import React, {useEffect} from "react";
 
-const NestedChild = () => {
-    const counter = useSelector(({counter: {value}}) => value)
-    const dispatch = useDispatch()
-    let input = []
-    // const inputInfo = []
-    // dispatch({
-    //     type: 'INC_CUSTOM'
-    // })
- return (
-    <header className="App-header">
-      <h1>{counter}</h1>
-        <button onClick={() => {
-        dispatch({type: 'INC'})}
-        }>inc</button>
-
-        <button onClick={() => {
-        dispatch({type: 'DEC'})}
-        }>DEC</button>
-
-        <button onClick={() => {
-        dispatch({type: 'RESET'})}
-        }>RESET</button>
-
-        <input/>
-
-         <button onClick={() => {
-        dispatch({type: 'INC_CUSTOM'})}
-        }>INC_CUSTOM</button>
-
-    </header>
-  )
+const fetchUsers = async () => {
+    const response = await fetch(https://jsonplaceholder.typicode.com/users)
 }
-
-
-
-function App() {
+useEffect(() => {fetchUsers()},[])
+export default function App() {
 
     return (
+
         <div>
-            <NestedChild/>
+            <Router>
+                <div>
+                    <Link to={'/users'}>users page</Link>
+                    <br/>
+                    <Link to={'/posts'}>posts page</Link>
+                    <br/>
+                    <Link to={'/comments'}>comments page</Link>
+                    <br/>
+
+                    <Route path={'/users'} render={() => <Users/>}/>
+                    <Route path={'/posts'} render={() => <Posts/>}/>
+                    <Route path={'/comments'} render={() => <Comments/>}/>
+                    <Route/>
+
+                </div>
+            </Router>
         </div>
 
     );
 }
-export default App
+
--- src/index.js
+++ src/index.js
@@ -3,60 +3,64 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import {createStore} from "redux";
-import {Provider} from "react-redux";
+import {BrowserRouter as Router, Link, Route} from "react-router-dom";
+import Users from "./Users/Users";
+import Posts from "./Posts/Posts";
+import Comments from "./Comments/Comments";
 
-
-
-
-const initialState = {
-    counter : {
-        value:0
-    },
-
-
-}
-const reducer = (state = initialState,action) => {
-    switch (action.type) {
-        case 'INC': {
-            return {...state,counter: {value:state.counter.value +1
-               }
-            }
-        }
-
-        case 'DEC': {
-            return {...state,counter: {value:state.counter.value -1
-                 }
-            }
-        }
-        case 'RESET': {
-            return {...state,counter: {value:state.counter.value = 0
-                 }
-            }
-        }
-        case 'INC_CUSTOM': {
-            return {...state,counter: {value:state.counter.value
-                 }
-            }
-        }
-        default : return state
-    }
-
-}
-
-const store= createStore(reducer)
-
-// const INCREMENT_ACTION ={
-//     type: 'INC'
+//
+//
+//
+//
+// const initialState = {
+//     counter : {
+//         value:0
+//     },
+//
+//
 // }
+// const reducer = (state = initialState,action) => {
+//     switch (action.type) {
+//         case 'INC': {
+//             return {...state,counter: {value:state.counter.value +1
+//                }
+//             }
+//         }
+//
+//         case 'DEC': {
+//             return {...state,counter: {value:state.counter.value -1
+//                  }
+//             }
+//         }
+//         case 'RESET': {
+//             return {...state,counter: {value:state.counter.value = 0
+//                  }
+//             }
+//         }
+//         case 'INC_CUSTOM': {
+//             return {...state,counter: {value:state.counter.value
+//                  }
+//             }
+//         }
+//         default : return state
+//     }
+//
+// }
+//
+// const store= createStore(reducer)
+//
+// // const INCREMENT_ACTION ={
+// //     type: 'INC'
+// // }
+//
+//
+//
 
 
 
 ReactDOM.render(
   <React.StrictMode>
-        <Provider store={store}>
              <App />
-        </Provider>
   </React.StrictMode>,
   document.getElementById('root')
 );
