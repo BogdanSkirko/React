27e0f0a6e3adf9792e288dbfffd0d321ccf8032a
Bohdan13011991 <Bogdanskirko@gmail.com>
2021-07-07 18:31:09 +0300

    redux-1

--- src/App.js
+++ src/App.js
@@ -1,52 +1,48 @@
-import User from "./components/Users/User";
-import {useState} from "react";
-
-let users = [
-    {name: 'vasya', age: 31, isMarried: false},
-    {name: 'petya', age: 30, isMarried: true},
-    {name: 'kolya', age: 29, isMarried: true},
-    {name: 'olya', age: 28, isMarried: false},
-    {name: 'max', age: 30, isMarried: true},
-    {name: 'anya', age: 31, isMarried: false},
-    {name: 'oleg', age: 28, isMarried: false},
-    {name: 'andrey', age: 29, isMarried: true},
-    {name: 'masha', age: 30, isMarried: true},
-    {name: 'olya', age: 31, isMarried: false},
-    {name: 'max', age: 31, isMarried: true}
-]
-export default function App() {
-
-    let [count,setCounter] = useState(0)
-    let [userInfo, setUsers] = useState(users)
-    let [dec,setDec] = useState(0)
-    const increment = () => setCounter(++count)
-    const decrement = () => setDec(--dec)
-    const deleteUsers = () => {
-        users.pop()
-        setUsers([...userInfo])
+import {useDispatch, useSelector} from "react-redux";
 
 
-    };
+const NestedChild = () => {
+    const counter = useSelector(({counter: {value}}) => value)
+    const dispatch = useDispatch()
+    let input = []
+    // const inputInfo = []
+    // dispatch({
+    //     type: 'INC_CUSTOM'
+    // })
+ return (
+    <header className="App-header">
+      <h1>{counter}</h1>
+        <button onClick={() => {
+        dispatch({type: 'INC'})}
+        }>inc</button>
+
+        <button onClick={() => {
+        dispatch({type: 'DEC'})}
+        }>DEC</button>
+
+        <button onClick={() => {
+        dispatch({type: 'RESET'})}
+        }>RESET</button>
+
+        <input/>
+
+         <button onClick={() => {
+        dispatch({type: 'INC_CUSTOM'})}
+        }>INC_CUSTOM</button>
+
+    </header>
+  )
+}
+
+
+
+function App() {
 
     return (
         <div>
-            <div>
-                count is {count}
-                <button onClick={increment}>increment</button>
-                   dec is {dec}
-                <button onClick={decrement}>decrement</button>
-            </div>
-            <div>
-                {
-                    users.map((value, index) =>
-                        <User key={index}
-                              {...value}
-                        />
-                    )
-                }
-
-                <button onClick={deleteUsers}>delete user</button>
-            </div>
+            <NestedChild/>
         </div>
+
     );
 }
+export default App
