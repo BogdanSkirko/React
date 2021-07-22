5c82885185b000aa0354f5886a76357be250cc3a
Bohdan13011991 <Bogdanskirko@gmail.com>
2021-07-22 17:06:22 +0300

    TypeScript#1

--- TS#1/FirstLesson.ts
+++ TS#1/FirstLesson.ts
@@ -19,15 +19,51 @@ class User {
         this.city = city;
         this.status = status;
     }
-    getName(): string {
-        return 'hello my name is ' + this.name
+    getName () {
+        return this.name
     }
-}
-let maksym = new User('maksym',323,'Lviv',true)
-console.log(maksym);
+    getAge () {
+        return this.age
+    }
+   getCity () {
+        return this.city
+    }
 
+    setName (newName) {
+        this.name = newName
+
+    }
+    setAge (newAge) {
+        this.age = newAge
+    }
+   setCity (newCity) {
+        this.city = newCity
+    }
+    changeStatus (newStatus) {
+        this.status = newStatus
+    }
+
+    }
+
+    let Dmytro = new User('Dima',33,'Lviv',false)
+    Dmytro.age = 3123
+        console.log(Dmytro);
 // 1) написать интерфейс Animal который описывает:
 // тип движения животного(плавает, ходит, бегает) типа стринг
 // что говорит тип стринг
 // и функцию которая возвращает информацию о животном
 // создать три класса Cat, Bird, Fish и имплементировать для каждого интерфейс Animal
+// interface Animal {
+//     swimming: string;
+//     walking: string;
+//     running: string
+// }
+//
+// class Cat implements Animal{
+//     sleep: number;
+//     eat: number;
+//     running: string;
+//     swimming: string;
+//     walking: string;
+//
+// }
